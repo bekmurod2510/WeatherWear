7c668c4a2848817ca560860febdc87c192dc7bc2
bekmurod2510 <bekmurod2510@google.com>
2025-12-03 22:47:48 +0500

    cookie settings fixed

--- backend/src/utils/cookie.utils.js
+++ backend/src/utils/cookie.utils.js
@@ -6,8 +6,8 @@
 const setCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000, // 1 day
     path: '/'
   });
